74840f53044ba16880a044cec7e5bfa25a930536
KMK <117750488+kmkimmel@users.noreply.github.com>
2022-11-14 15:06:30 -0600

    Update streamlit_app.py
    fixed error added () after fetchall

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ streamlit.header("The fruit load list contains:|")
 def get_fruit_load_list():
     with my_cnx.cur() as my_cur:
          my_cur.execute("SELECT * from fruit_load_list")
-         return my_cur.fetchall
+         return my_cur.fetchall()
     
 # Add a button to load the fruit
 If streamlit.button('Get Fruit Load List'):
