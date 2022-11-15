58a3d39b9de6de341fd57bfe03c9f5523ebc7855
KMK <117750488+kmkimmel@users.noreply.github.com>
2022-11-15 10:31:29 -0600

    Update streamlit_app.py
    changed button name from Get Fruit Load List to Get Fruit List  and add closes to snowflake connects - my_cnx.close()

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,21 +52,23 @@ def get_fruit_load_list():
          return my_cur.fetchall()
     
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
 
 # Allow the enduser to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into fruit_load_list values ('"+add_my_fruit+"')")
+         my_cur.execute("insert into fruit_load_list values ('"+ add_my_fruit +"')")
          return "Thanks for adding " + new_fruit
     
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(new_fruit)
+    my_cnx.close()
     streamlit_text(back_from_function)
 # don't run anything past here while we troubleshoot
 #streamlit.stop()
